5ac900aeb9aa381595bac40ab9e3425df85d8df1
KCharih <kcharih@arfp.asso.fr>
2019-12-16 14:35:25 +0100

    Second message

--- Projet1/src/HelloWorld.java
+++ Projet1/src/HelloWorld.java
@@ -16,7 +16,9 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		// TODO Ajouter des commentaires
-		System.out.println("Hello World");		 
+		System.out.println("Hello World");	
+		System.out.println("My second message");
+		
 	}
 
 }
