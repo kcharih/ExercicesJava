944c8ea69c5af73be4054629f2e12003c9369338
KCharih <kcharih@arfp.asso.fr>
2019-12-16 14:33:25 +0100

    Generate Javadoc

---
+++ Projet1/src/HelloWorld.java
@@ -0,0 +1,22 @@
+/**
+ * Classe d'affichage d'un message utilisateur
+ */
+
+/**
+ * @author Dev1
+ * @version 1.0
+ * @since 2019
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @author Dev2
+	 * @param args paramètres du programme
+	 */
+	public static void main(String[] args) {
+		// TODO Ajouter des commentaires
+		System.out.println("Hello World");		 
+	}
+
+}
