bfa119831b0220114427f276d2ea6950c16db7cf
KCharih <kcharih@arfp.asso.fr>
2019-12-16 14:40:11 +0100

    update javadoc

--- Projet1/src/Test.java
+++ Projet1/src/Test.java
@@ -1,4 +1,12 @@
-
+/**
+ * @author Dev1
+ * @version 1.0
+ * @since 2019
+ *
+ */
 public class Test {
-
+	public static void main(String[] args) {
+		// TODO Ajouter des commentaires
+		System.out.println("Hello World");			
+	}
 }
