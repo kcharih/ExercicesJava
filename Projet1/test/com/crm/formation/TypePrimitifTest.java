d6063e9bb16a8bd330c9fa04cc132f15e9266a11
KCharih <kcharih@arfp.asso.fr>
2019-12-20 11:23:24 +0100

    Creation de la classe TypePrimitifTest

---
+++ Projet1/test/com/crm/formation/TypePrimitifTest.java
@@ -0,0 +1,46 @@
+/**
+ * Classe de test pour les types primitifs
+ */
+package com.crm.formation;
+
+/**
+ * @author KC
+ *
+ */
+public class TypePrimitifTest {
+
+	/**
+	 * @param args parametres du pgm
+	 */
+	public static void main(String[] args) {
+		/* Decalration et initialisation des variables */
+
+		// Entiers
+		byte vByte = 12;
+		short vShort = 1258;
+		int vInt = 1258977;
+		long vLong = 25568774112L;
+
+		// Reels
+		float vFloat = 15.69F;
+		double vDouble = 2566874.3654;
+
+		// Caractere
+		char vChar = 'A';
+
+		// Booleen
+		boolean vBool = true;
+
+		// Affichage des variables
+		System.out.println("Ma variable de type byte vaut : " + vByte);
+		System.out.println("Ma variable de type short vaut : " + vShort);
+		System.out.println("Ma variable de type int vaut : " + vInt);
+		System.out.println("Ma variable de type long vaut : " + vLong);
+		System.out.println("Ma variable de type float vaut : " + vFloat);
+		System.out.println("Ma variable de type double vaut : " + vDouble);
+		System.out.println("Ma variable de type char vaut : " + vChar);
+		System.out.println("Ma variable de type booleen vaut : " + vBool);
+
+	}
+
+}
